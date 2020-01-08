651efa7a59f0c947311c31b88fa18d0b5b749cf8
samjoosten <31620260+samjoosten@users.noreply.github.com>
2020-01-08 10:27:27 +0100

    changed the login style

--- screens/Login.js
+++ screens/Login.js
@@ -1,110 +1,117 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {
-  TouchableHighlight,
-  TextInput,
-  View,
-  StyleSheet,
-  KeyboardAvoidingView,
-  Text,
-  Dimensions
+    TouchableHighlight,
+    TextInput,
+    View,
+    StyleSheet,
+    KeyboardAvoidingView,
+    Text,
+    Dimensions
 } from "react-native";
 import styles from "../style";
 
 let scr = Dimensions.get("window").width;
 
 export default class Login extends Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: "Login",
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#474747"
-      }
-    };
-  };
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: "",
-      password: ""
+    static navigationOptions = ({navigation}) => {
+        return {
+            title: "Login",
+            headerTintColor: "white",
+            headerStyle: {
+                backgroundColor: "#474747"
+            }
+        };
     };
 
-    this.loginSuccessful = this.loginSuccessful.bind(this);
-  }
+    constructor(props) {
+        super(props);
 
-  onLogin() {
-    //check name and password
+        this.state = {
+            name: "",
+            password: ""
+        };
 
-    this.props.navigation.navigate("DashboardDomain");
+        this.loginSuccessful = this.loginSuccessful.bind(this);
+    }
 
-    // const account = {
-    //     name: this.state.name,
-    //     password: this.state.password
-    // };
+    onLogin() {
+        //check name and password
 
-    // if(account.name == "" || account.password == ""){
-    //     alert("Name and password cannot be empty!")
-    //     return;
-    // }
+        this.props.navigation.navigate("DashboardDomain");
 
-    // const url = Constants.url + 'login';
+        // const account = {
+        //     name: this.state.name,
+        //     password: this.state.password
+        // };
 
-    // fetch(url, {
-    //     method: 'POST',
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(account)
-    // })
-    //     .then((response) => response.json())
-    //     .then((responseJson) => {
+        // if(account.name == "" || account.password == ""){
+        //     alert("Name and password cannot be empty!")
+        //     return;
+        // }
 
-    //         //check if passwords are the same
-    //         if(responseJson.result){
-    //             this.loginSuccessful();
-    //         }else{
-    //             alert('login failed');
-    //         }
-    //     })
-    //     .catch((error) => {
-    //         console.error(JSON.stringify(error));
-    //     });
-  }
+        // const url = Constants.url + 'login';
 
-  //todo: go to next screen.
-  loginSuccessful() {
-    alert("login successful");
-  }
+        // fetch(url, {
+        //     method: 'POST',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(account)
+        // })
+        //     .then((response) => response.json())
+        //     .then((responseJson) => {
 
-  render() {
-    return (
-      <KeyboardAvoidingView style={styles.login_container} behavior="padding">
-        <View>
-          <TextInput
-            value={this.state.name}
-            onChangeText={name => this.setState({ name })}
-            placeholder={"Name"}
-            placeholderTextColor="#C3C3C3"
-            style={styles.login_input}
-          />
-          <TextInput
-            value={this.state.password}
-            onChangeText={password => this.setState({ password })}
-            placeholder={"Password"}
-            placeholderTextColor="#C3C3C3"
-            secureTextEntry={true}
-            style={styles.login_input}
-          />
-          <TouchableHighlight
-            style={styles.login_button}
-            onPress={this.onLogin.bind(this)}
-          >
-            <Text style={styles.login_btnText}>Login</Text>
-          </TouchableHighlight>
-        </View>
-      </KeyboardAvoidingView>
-    );
-  }
+        //         //check if passwords are the same
+        //         if(responseJson.result){
+        //             this.loginSuccessful();
+        //         }else{
+        //             alert('login failed');
+        //         }
+        //     })
+        //     .catch((error) => {
+        //         console.error(JSON.stringify(error));
+        //     });
+    }
+
+    //todo: go to next screen.
+    loginSuccessful() {
+        alert("login successful");
+    }
+
+    render() {
+        return (
+            <KeyboardAvoidingView style={styles.login_container} behavior="padding">
+                <View style={styles.form}>
+                    <View>
+                        <Text style={styles.inputTitle}>Name</Text>
+                        <TextInput
+                            value={this.state.name}
+                            onChangeText={name => this.setState({name})}
+                            placeholderTextColor="#C3C3C3"
+                            style={styles.textInput}
+                        />
+                    </View>
+
+                    <View style={{marginTop: 32}}>
+                        <Text style={styles.inputTitle}>Password</Text>
+                        <TextInput
+                            value={this.state.password}
+                            onChangeText={password => this.setState({password})}
+                            placeholderTextColor="#C3C3C3"
+                            secureTextEntry={true}
+                            style={styles.textInput}
+                        />
+                    </View>
+                </View>
+
+                <TouchableHighlight
+                    style={styles.loginButton}
+                    onPress={this.onLogin.bind(this)}
+                >
+                    <Text style={{color: "#fff", fontWeight: "500"}}>Login</Text>
+                </TouchableHighlight>
+            </KeyboardAvoidingView>
+        );
+    }
 }
--- style.js
+++ style.js
@@ -1,198 +1,223 @@
-import { StyleSheet, Dimensions } from "react-native";
+import {StyleSheet, Dimensions} from "react-native";
 
 let scr = Dimensions.get("window").width;
 
 export default StyleSheet.create({
-  container: {
-    backgroundColor: "#1d2637",
-    height: "100%"
-  },
-  questionContainer: {
-    backgroundColor: "#fff",
-    height: "100%"
-  },
-  ptb10: {
-    marginVertical: 10
-  },
-  titles_charts: {
-    fontSize: 20
-  },
-  axis_titles: {
-    color: "#fff"
-  },
-  text_white: {
-    color: "#fff"
-  },
-  text_white_opacity: {
-    color: "#ffffff5c"
-  },
-  text_h2: {
-    fontSize: 30
-  },
-  text_h3: {
-    fontSize: 20
-  },
-  text_h4: {
-    fontSize: 18
-  },
-  mlr20: {
-    marginHorizontal: 20
-  },
-  mlr10: {
-    marginHorizontal: 10
-  },
-  pt5: {
-    paddingTop: 5
-  },
-  pb15: {
-    paddingBottom: 15
-  },
-  mt15: {
-    marginTop: 15
-  },
-  mt55: {
-    marginTop: 55
-  },
-  mtb25: {
-    marginVertical: 25
-  },
-  mb50: {
-    marginBottom: 220
-  },
-  ptb15: {
-    paddingVertical: 15
-  },
-  login_container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#313131"
-  },
-  login_input: {
-    backgroundColor: "white",
-    width: scr * 0.75,
-    height: 52,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: "black",
-    fontSize: 16,
-    marginBottom: 10
-  },
-  login_button: {
-    width: scr * 0.75,
-    height: 52,
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 10,
-    backgroundColor: "#409eff"
-  },
-  login_btnText: {
-    textAlign: "center",
-    textAlignVertical: "center",
-    fontSize: 18,
-    color: "white"
-  },
-  profile_panel: {
-    backgroundColor: "#fff",
-    borderRadius: 5,
-    padding: 10,
-    margin: 15
-  },
-  profile_image: {
-    width: 150,
-    height: 150
-  },
-  btnAddPanel: {
-    marginHorizontal: 15,
-    flex: 1,
-    justifyContent: "center"
-  },
-  btnAddApp: {},
-  feedbackStyle: {
-    borderWidth: 0.5,
-    borderTopColor: "#a5a5a53b",
-    borderRightWidth: 0,
-    borderLeftWidth: 0,
-    borderBottomWidth: 0
-  },
-  badge: {
-    marginLeft: 10,
-    paddingVertical: 5,
-    paddingHorizontal: 25,
-    borderRadius: 15,
-    backgroundColor: "#feaa0a"
-  },
-  pos_rel: {
-    position: "relative"
-  },
-  smiley_icon_position: {
-    position: "absolute",
-    right: 10
-  },
-  image_list_icon: {
-    position: "absolute",
-    right: 0,
-    top: 27,
-    backgroundColor: "#feaa0a",
-    borderRadius: 30,
-    padding: 10
-  },
-  osLogo: {
-    width: 30,
-    height: 30
-  },
-  panel: {
-    backgroundColor: "#232e40",
-    padding: 30,
-    borderRadius: 15,
-    marginBottom: 15,
-    elevation: 4
-  },
-  panel_Dashboard: {
-    backgroundColor: "#232e40",
-    height: 460,
-    width: Dimensions.get("window").width,
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 15,
-    elevation: 4
-  },
-  text_bold: {
-    fontWeight: "bold"
-  },
-  text_italic: {
-    fontStyle: "italic"
-  },
-  panel_header: {
-    paddingVertical: 15
-  },
-  hr: {
-    borderBottomColor: "#ffffff5c",
-    borderBottomWidth: 1
-  },
-  center_objects: {
-    justifyContent: "center",
-    backgroundColor: "#388e3c",
-    paddingHorizontal: 30,
-    marginLeft: 5
-  },
-  btnDelete: {
-    flexDirection: "row",
-    backgroundColor: "#fb3d57",
-    justifyContent: "center",
-    paddingVertical: 10,
-    borderRadius: 10
-  },
-  btnDeleteTextSize: {
-    fontSize: 14
-  },
-  btnDeletePos: {
-    marginTop: 5,
-    marginHorizontal: 10
-  },
-  filterDropdown: {
-    color: "#fff",
-    backgroundColor: "#33415f",
-    height: 40
-  }
+    container: {
+        backgroundColor: "#1d2637",
+        height: "100%"
+    },
+    questionContainer: {
+        backgroundColor: "#fff",
+        height: "100%"
+    },
+    ptb10: {
+        marginVertical: 10
+    },
+    titles_charts: {
+        fontSize: 20
+    },
+    axis_titles: {
+        color: "#fff"
+    },
+    text_white: {
+        color: "#fff"
+    },
+    text_white_opacity: {
+        color: "#ffffff5c"
+    },
+    text_h2: {
+        fontSize: 30
+    },
+    text_h3: {
+        fontSize: 20
+    },
+    text_h4: {
+        fontSize: 18
+    },
+    mlr20: {
+        marginHorizontal: 20
+    },
+    mlr10: {
+        marginHorizontal: 10
+    },
+    pt5: {
+        paddingTop: 5
+    },
+    pb15: {
+        paddingBottom: 15
+    },
+    mt15: {
+        marginTop: 15
+    },
+    mt55: {
+        marginTop: 55
+    },
+    mtb25: {
+        marginVertical: 25
+    },
+    mb50: {
+        marginBottom: 220
+    },
+    ptb15: {
+        paddingVertical: 15
+    },
+    login_container: {
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#313131"
+    },
+    login_input: {
+        backgroundColor: "white",
+        width: scr * 0.75,
+        height: 52,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "black",
+        fontSize: 16,
+        marginBottom: 10
+    },
+    // login_button: {
+    //     width: scr * 0.75,
+    //     height: 52,
+    //     padding: 10,
+    //     marginBottom: 10,
+    //     borderRadius: 10,
+    //     backgroundColor: "#409eff"
+    // },
+    login_btnText: {
+        textAlign: "center",
+        textAlignVertical: "center",
+        fontSize: 18,
+        color: "white"
+    },
+    profile_panel: {
+        backgroundColor: "#fff",
+        borderRadius: 5,
+        padding: 10,
+        margin: 15
+    },
+    profile_image: {
+        width: 150,
+        height: 150
+    },
+    btnAddPanel: {
+        marginHorizontal: 15,
+        flex: 1,
+        justifyContent: "center"
+    },
+    btnAddApp: {},
+    feedbackStyle: {
+        borderWidth: 0.5,
+        borderTopColor: "#a5a5a53b",
+        borderRightWidth: 0,
+        borderLeftWidth: 0,
+        borderBottomWidth: 0
+    },
+    badge: {
+        marginLeft: 10,
+        paddingVertical: 5,
+        paddingHorizontal: 25,
+        borderRadius: 15,
+        backgroundColor: "#feaa0a"
+    },
+    pos_rel: {
+        position: "relative"
+    },
+    smiley_icon_position: {
+        position: "absolute",
+        right: 10
+    },
+    image_list_icon: {
+        position: "absolute",
+        right: 0,
+        top: 27,
+        backgroundColor: "#feaa0a",
+        borderRadius: 30,
+        padding: 10
+    },
+    osLogo: {
+        width: 30,
+        height: 30
+    },
+    panel: {
+        backgroundColor: "#232e40",
+        padding: 30,
+        borderRadius: 15,
+        marginBottom: 15,
+        elevation: 4
+    },
+    panel_Dashboard: {
+        backgroundColor: "#232e40",
+        height: 460,
+        width: Dimensions.get("window").width,
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 15,
+        elevation: 4
+    },
+    text_bold: {
+        fontWeight: "bold"
+    },
+    text_italic: {
+        fontStyle: "italic"
+    },
+    panel_header: {
+        paddingVertical: 15
+    },
+    hr: {
+        borderBottomColor: "#ffffff5c",
+        borderBottomWidth: 1
+    },
+    center_objects: {
+        justifyContent: "center",
+        backgroundColor: "#388e3c",
+        paddingHorizontal: 30,
+        marginLeft: 5
+    },
+    btnDelete: {
+        flexDirection: "row",
+        backgroundColor: "#fb3d57",
+        justifyContent: "center",
+        paddingVertical: 10,
+        borderRadius: 10
+    },
+    btnDeleteTextSize: {
+        fontSize: 14
+    },
+    btnDeletePos: {
+        marginTop: 5,
+        marginHorizontal: 10
+    },
+    form: {
+        marginBottom: 48,
+        marginHorizontal: 30
+    },
+    inputTitle: {
+        color: "#fff",
+        fontSize: 10,
+        textTransform: "uppercase"
+
+    },
+    textInput: {
+        height: 40,
+        borderBottomColor: "#8A8F9E",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        fontSize: 15,
+        color: "#161F3D"
+    },
+    filterDropdown: {
+        color: "#fff",
+        backgroundColor: "#33415f",
+        height: 40
+    },
+    loginButton: {
+        marginHorizontal: 30,
+        backgroundColor: "#409eff",
+        borderRadius: 4,
+        height: 52,
+        alignItems: "center",
+        justifyContent: "center"
+
+    }
 });
